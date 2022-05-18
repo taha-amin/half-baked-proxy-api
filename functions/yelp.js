068a7df5e1911e3d9dddcff69c43702a92723119
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-18 09:43:23 -0700

    was able to consult the yelp docs to figure out how to use search parameters to make a request for businesses and in app.js implemented this using a const response that awaits the yelp api quiry string along with the searchFilter. The api key is in the .env file so simply calling process.env.YELP_KEY is enough in my authorization. In yelpsearch.js, tracked state for data, query, and spinner and in handleyelpsubmit func used fetch to make a request to netlify yelp funciton and made sure to pass the search query as a query param in the URL. in return statement made the fetch on submit then made a ternary for spinner and mapped through the yelpdata

--- functions/yelp.js
+++ functions/yelp.js
@@ -1,27 +1,36 @@
+const fetch = require('node-fetch');
 require('dotenv').config();
 
 const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'Content-Type',
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
 };
-
 
 exports.handler = async (event, context) => {
   try {
     // grab the city, state, and country from the request's query parameters
     // here is an example from the netlify docs:
-    // https://functions.netlify.com/playground/#hello%2C-%7Bname%7D 
+    // https://functions.netlify.com/playground/#hello%2C-%7Bname%7D
 
     // consult the yelp docs to figure out how to use a city, state, and country to make a request for businesses
     // https://www.yelp.com/developers/documentation/v3/business_search
     // don't forget to add the yelp API key!
-    
-    return { 
-      statusCode: 200, 
+    const response = await fetch(
+      `https://api.yelp.com/v3/businesses/search?location=${event.queryStringParameters.searchFilter}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.YELP_KEY}`,
+        },
+      }
+    );
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
       headers,
-    // this is where you shoot data back to the user. right now it's sending an empty object--replace this with the yelp data. remember, you do need to stringify it, otherwise netlify gets mad. ¯\_(ツ)_/¯
-      body: JSON.stringify({}),
+      // this is where you shoot data back to the user. right now it's sending an empty object--replace this with the yelp data. remember, you do need to stringify it, otherwise netlify gets mad. ¯\_(ツ)_/¯
+      body: JSON.stringify({ data }),
     };
   } catch (error) {
     console.log(error);
--- src/YelpSearch.js
+++ src/YelpSearch.js
@@ -1,27 +1,49 @@
 import { useState } from 'react';
+import { getYelpData } from './services/fetch-utils';
+import Spinner from './Spinner';
 
 export default function YelpSearch() {
-    // you'll need to track your yelp search results, the loading state, and a form field for location with a default value.
+  // you'll need to track your yelp search results, the loading state, and a form field for location with a default value.
+  const [yelpData, setYelpData] = useState([]);
+  const [yelpQuery, setYelpQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   async function handleYelpSubmit(e) {
     e.preventDefault();
-  
+
     // set the loading state to true
+    setIsLoading(true);
+
     // use fetch to make a request to your netlify yelp function. Be sure to pass the search query as a query param in the URL
-  
+    const response = await getYelpData(yelpQuery);
+
     // put the jsonified data in state and set the loading state to false
+    setYelpData(response.data.businesses);
+    setIsLoading(false);
   }
-  
+
   return (
-    <section className='yelp'>
+    <section className="yelp">
       {/* make the fetch on submit */}
-      <form>
+      <form onSubmit={handleYelpSubmit}>
+        <input onChange={(e) => setYelpQuery(e.target.value)} />
         Search yelp for a city
         {/* add inputs/labels for city name, state, and country, using all the things we need with react forms. Don't forget to use the value property to sync these up with the default values in react state */}
         <button>Search yelp</button>
       </form>
       {/* Make a BusinessesList component to import and use here. Use a ternery to display a loading spinner (make a <Spinner /> component for this) if the data is still loading. */}
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        yelpData.map(({ name, price, phone, image_url }, i) => (
+          <div key={name + i}>
+            <h2>{name}</h2>
+            <p>${price}</p>
+            <p>{phone}</p>
+            <img src={image_url} />
+          </div>
+        ))
+      )}
     </section>
   );
 }
-  
